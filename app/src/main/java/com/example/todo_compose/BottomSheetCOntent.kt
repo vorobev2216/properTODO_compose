3db675044b626c16f6eadd6d54257d0ba50e3843
Aboltus <sotikin0406@gmail.com>
2024-06-24 01:09:58 +0300

    DatePicker added.

--- app/src/main/java/com/example/todo_compose/BottomSheetCOntent.kt
+++ app/src/main/java/com/example/todo_compose/BottomSheetCOntent.kt
@@ -1,5 +1,7 @@
 package com.example.todo_compose
 
+import android.app.DatePickerDialog
+import android.content.res.Resources.Theme
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -12,13 +14,21 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -30,6 +40,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -56,6 +67,8 @@ fun BottomSheetContent() {
         fontSize = 16.sp, // Adjust font size as needed
     )
 
+    var showDDL by remember { mutableStateOf(false) }
+
     Column(
         Modifier
             .fillMaxSize()
@@ -65,7 +78,11 @@ fun BottomSheetContent() {
     ) {
         TextField(
             label = {
-                Text(text = "New Task", fontFamily = FontFamily(Font(R.font.metropolissemibold)), color = Color.White)
+                Text(
+                    text = "New Task",
+                    fontFamily = FontFamily(Font(R.font.metropolissemibold)),
+                    color = Color.White
+                )
             },
             value = textFieldTitle,
             modifier = Modifier
@@ -88,7 +105,11 @@ fun BottomSheetContent() {
 
         TextField(
             label = {
-                Text(text = "Description", fontFamily = FontFamily(Font(R.font.metropolissemibold)), color = Color.White)
+                Text(
+                    text = "Description",
+                    fontFamily = FontFamily(Font(R.font.metropolissemibold)),
+                    color = Color.White
+                )
             },
             value = textFieldDesc,
             modifier = Modifier
@@ -105,12 +126,35 @@ fun BottomSheetContent() {
             )
 
         )
-        Spacer(modifier = Modifier
-            .fillMaxWidth()
-            .height(50.dp))
-        Row {
-            CardInAddScreen(text = "Today", icon = R.drawable.baseline_calendar_today_24 )
-            CardInAddScreen(text = "Category", icon = R.drawable.baseline_category_24 )
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.Start,
+
+
+                ) {
+                val myDatePickerDialog = DatePickerDialog(LocalContext.current)
+                CardInAddScreen(text = "Today", icon = R.drawable.baseline_calendar_today_24) {
+                    myDatePickerDialog.show()
+                }
+                CardInAddScreen(text = "Category", icon = R.drawable.baseline_category_24) {
+
+                    showDDL = !showDDL
+                }
+                if (showDDL) {
+                    DDL()
+                }
+            }
+            RoundAddButton()
         }
 
     }
@@ -118,12 +162,59 @@ fun BottomSheetContent() {
 
 
 @Composable
-fun CardInAddScreen(text: String, icon: Int){
-    Card(colors = CardDefaults.cardColors(Color.DarkGray), modifier = Modifier.height(50.dp).width(120.dp)){
-        Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxSize()) {
+fun CardInAddScreen(text: String, icon: Int, onCardClick: () -> Unit) {
+    Card(
+        colors = CardDefaults.cardColors(Color.DarkGray),
+        modifier = Modifier
+            .height(55.dp)
+            .width(130.dp)
+            .fillMaxWidth()
+            .padding(start = 5.dp)
+            .wrapContentWidth()
+            .clickable { onCardClick() }
+
+
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp)
+        ) {
             Icon(painter = painterResource(id = icon), contentDescription = null)
             Text(text = text, fontFamily = FontFamily(Font(R.font.metropolissemibold)))
         }
     }
 
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DDL() {
+    var isExpand by remember {
+        mutableStateOf(false)
+    }
+
+    ExposedDropdownMenuBox(expanded = isExpand, onExpandedChange = { isExpand = it }) {
+        ExposedDropdownMenu(expanded = isExpand, onDismissRequest = { isExpand = false }) {
+            DropdownMenuItem(
+                text = { Text(text = "Important", color = Color(0xFF7E0D2E)) },
+                onClick = { isExpand = false })
+            DropdownMenuItem(
+                text = { Text(text = "Need to do", color = Color(0xFFAD8405)) },
+                onClick = { isExpand = false })
+            DropdownMenuItem(
+                text = { Text(text = "Can wait", color = Color(0xFF408B0A)) },
+                onClick = { isExpand = false })
+
+        }
+    }
+}
+
+@Composable
+fun ShowDDL() {
+    // Вызываем вашу Composable функцию DDL()
+    DDL()
 }
--- app/src/main/java/com/example/todo_compose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/todo_compose/ui/theme/Theme.kt
@@ -14,7 +14,7 @@ import androidx.compose.ui.platform.LocalContext
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Red80
 )
 
 private val LightColorScheme = lightColorScheme(
